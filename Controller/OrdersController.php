ca4f4e115bf15e6b959d8b055259c342918aec4d
Simon Yorkston <simon@toyfoundry.com>
2015-02-04 23:00:39 +0100

    Pricings Tweak

--- Controller/OrdersController.php
+++ Controller/OrdersController.php
@@ -46,6 +46,23 @@ class OrdersController extends AppController {
     }
 
     public function add() {
+
+        //Sets pricing
+        $this->loadModel('Size');
+        $this->loadModel('Price');
+        $this->loadModel('Extra');
+        $this->Extra->Behaviors->load('Containable');
+
+        $sizes = $this->Size->find('list');
+        $extras = $this->Extra->find('first', ['link' => 'ExtraPrice','conditions' => ['ExtraPrice.currency_id' => $this->viewVars['currency']['id']]]);
+        $this->set('extras',$extras);
+
+        foreach ($sizes as $id => $size) {
+            $price = $this->Price->find('list',['conditions' => ['size_id' => $id, 'currency_id' => $this->viewVars['currency']['id']]]);
+            $returnArray[$size] = reset($price);
+        }
+        $this->set('prices',$returnArray);
+
         if ($this->request->is('post')) {
             if ($this->Order->saveAll( $this->request->data, array('validate' => 'only'))) {
 
@@ -198,22 +215,6 @@ class OrdersController extends AppController {
                 $this->Session->setFlash(__('The order could not be saved. Please, try again.'));
             }*/
         }
-
-        //Sets pricing
-        $this->loadModel('Size');
-        $this->loadModel('Price');
-        $this->loadModel('Extra');
-        $this->Extra->Behaviors->load('Containable');
-
-        $sizes = $this->Size->find('list');
-        $extras = $this->Extra->find('first', ['link' => 'ExtraPrice','conditions' => ['ExtraPrice.currency_id' => $this->viewVars['currency']['id']]]);
-        $this->set('extras',$extras);
-
-        foreach ($sizes as $id => $size) {
-            $price = $this->Price->find('list',['conditions' => ['size_id' => $id, 'currency_id' => $this->viewVars['currency']['id']]]);
-            $returnArray[$size] = reset($price);
-        }
-        $this->set('prices',$returnArray);
 
     }
 
